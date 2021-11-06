4d4488629156ae8e44a0b82e0463976b38d4e242
toofus <mail@dhruba.xyz>
2021-11-06 13:26:10 +0530

    wip

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}

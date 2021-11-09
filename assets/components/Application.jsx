b6a787e0da6e628d2c91ff582a1a62a949ef7cab
toofus <mail@dhruba.xyz>
2021-11-09 09:29:33 +0530

    headless ui menu works

--- assets/components/Application.jsx
+++ assets/components/Application.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, lazy, Suspense } from "react";
+import React, { useContext, lazy, Suspense, useState, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import {
   BrowserRouter,
   Link,
@@ -20,21 +21,89 @@ const Application = () => {
     menu.open = !menu.open;
   };
 
+  const handleClickDrop = (path) => {
+    navigate(path);
+  };
+
   return (
     <>
-      <div className={menu.open ? "block my-5" : "hidden"}>Menu Open</div>
-      <div className="min-h-screen bg-gray-100">
-        <ul className="flex items-center space-x-2">
-          <li>
-            <button onClick={() => handleClick("/")}>Dashboard</button>
-          </li>
-          <li>
-            <button onClick={() => handleClick("/about")}>About</button>
-          </li>
-          <li>
-            <button onClick={() => handleClick("/profile")}>Profile</button>
-          </li>
-        </ul>
+      <div className="min-h-screen bg-gray-100 container mx-auto">
+        <Menu as="div" className="lg:hidden relative bg-green-200">
+          <Menu.Button className="p-2 block w-full">Open</Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              as="ul"
+              className="absolute w-full bg-white flex flex-col divide-y p-3"
+            >
+              <Menu.Item as="li">
+                <button onClick={() => handleClickDrop("/about")}>About</button>
+              </Menu.Item>
+              <Menu.Item as="li">
+                <button onClick={() => handleClickDrop("/profile")}>
+                  Profile
+                </button>
+              </Menu.Item>
+              <Menu.Item as="li">
+                <button>Home</button>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+
+        <div className="hidden lg:flex items-center justify-between space-x-2 p-2 bg-red-200">
+          <ul className="flex-1 flex items-center space-x-2 bg-blue-200 p-2">
+            <li>
+              <button onClick={() => handleClick("/")}>Dashboard</button>
+            </li>
+            <li>
+              <button onClick={() => handleClick("/about")}>About</button>
+            </li>
+            <li>
+              <button onClick={() => handleClick("/profile")}>Profile</button>
+            </li>
+          </ul>
+
+          <Menu as="div" className="relative bg-green-200">
+            <Menu.Button className="p-2">Open</Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items
+                as="ul"
+                className="absolute right-0 w-56 origin-top-right bg-white flex flex-col divide-y p-3"
+              >
+                <Menu.Item as="li">
+                  <button onClick={() => handleClickDrop("/about")}>
+                    About
+                  </button>
+                </Menu.Item>
+                <Menu.Item as="li">
+                  <button onClick={() => handleClickDrop("/profile")}>
+                    Profile
+                  </button>
+                </Menu.Item>
+                <Menu.Item as="li">
+                  <button>Home</button>
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+
         <div className="mt-10">
           <Suspense fallback={<p>Loading...</p>}>
             <Routes>
